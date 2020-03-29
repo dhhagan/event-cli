3c877350feee6c637a151c8f89662868f8e16b80
David H Hagan <david@davidhhagan.com>
2020-03-29 14:22:00 -0400

    added support for unittesting

--- event/cli.py
+++ event/cli.py
@@ -52,6 +52,70 @@ def log(port, baud, timeout, fpath, stream):
                     print (rv)
 
 
+@click.command("test")
+@click.option("--port", default=None, help="The port the Arduino is connected to", type=str, required=False)
+@click.option("--delay", default=20, type=int, help="The number of seconds each test should last")
+@click.option("--baud", default=115200, help="The baudrate", type=int, required=False)
+@click.option("--timeout", default=1, help="The serial timeout", type=int, required=False)
+@click.option("--fpath", default="test-steps.csv", help="The file path where data will be stored", type=str)
+def test(delay, port, baud, timeout, fpath):
+    """Run unittests on the hardware
+    """
+    import itertools
+    from datetime import datetime
+    import serial
+    import time
+
+    # ie_options = [1, 2, 3, 4]
+    # tv_options = [100, 200, 300, 400, 500, 600, 700, 800]
+    # bpm_options = [10, 15, 20, 25, 30, 35, 40]
+
+    ie_options = [2, ]
+    tv_options = [250, 750]
+    bpm_options = [10, 30]
+
+    options = list(itertools.product(*[ie_options, tv_options, bpm_options]))
+
+    # how long will this test take?
+    total_time_min = len(options)*delay / 60.
+
+    print ("This test will take {} min".format(total_time_min))
+    print ("Starting now...")
+
+    # set up a file to write to
+    # setup the serial connection
+    try:
+        ser = serial.Serial(port, baud, timeout=timeout)
+    except Exception as e:
+        sys.exit("Serial connection setup failed")
+        print (e)
+
+    # open a file for writing
+    with open(fpath, "w") as fobj:
+        # write a header
+        fobj.write("timestamp_local,step_no,ie_ratio,tidal_vol,bpm\n")
+        fobj.flush()
+
+        for i, (ie_ratio, tidal_vol, bpm) in enumerate(options):
+            print ("Step {}/{}\tIE = {:.2f}\tTV = {:.2f}\tBPM = {:.0f}".format(i+1, len(options), ie_ratio, tidal_vol, bpm))
+
+            # write the step to file
+            fobj.write("{},{},{:.2f},{:.0f},{:.1f}\n".format(str(datetime.now()), i+1, ie_ratio, tidal_vol, bpm))
+
+            tidal_vol_cmd = "v{:d}".format(tidal_vol)
+            ie_ratio_cmd = "e{:d}".format(ie_ratio)
+            bpm_cmd = "b{:d}".format(bpm)
+
+            # write the step to the arduino
+            ser.write(tidal_vol_cmd.encode("utf-8"))
+            ser.write(ie_ratio_cmd.encode("utf-8"))
+            ser.write(bpm_cmd.encode("utf-8"))
+
+            # delay
+            time.sleep(delay)
+
+
 
 
 cli.add_command(log)
+cli.add_command(test)
